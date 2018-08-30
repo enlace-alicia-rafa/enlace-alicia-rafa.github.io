81e83fbc53151a739652d8901cf0dc8543147b45
Rafael Minguet <rminguet@gmail.com>
2018-08-30 03:58:25 +0200

    Deploy 1.6.1

---
+++ precache-manifest.64c6d46c17b611ba8dddf842a4843dcf.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "616c3db5abe415dad140fc443c770349",
+    "url": "/css/tachyons-custom.css"
+  },
+  {
+    "revision": "91e4b142862ceb7d6b40f974a98fba99",
+    "url": "/img/cards/van.jpg"
+  },
+  {
+    "revision": "f1b36aed77c13e111f82",
+    "url": "/js/chunk-vendors.6d5a51dd.js"
+  },
+  {
+    "revision": "9152d7f1724ed8fbcd2e0c87029f193c",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "993d8abda8051d48fe0aa8cc260b0060",
+    "url": "/img/corrugated_texture.993d8abd.jpg"
+  },
+  {
+    "revision": "5e6402530097699fdbbf578559d19e30",
+    "url": "/img/cards/welcome.jpg"
+  },
+  {
+    "revision": "0323f6eb990c309a82e2b20bf3c0bc80",
+    "url": "/img/corrugated_texture-big.0323f6eb.jpg"
+  },
+  {
+    "revision": "b62334fbd3c092158aae",
+    "url": "/js/app.9a56de8f.js"
+  },
+  {
+    "revision": "e4d13275f709c1ee0c7e7c7061ee044b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0836a7092d9abdd9cb606306ccee3159",
+    "url": "/img/cards/loading.gif"
+  },
+  {
+    "revision": "8a8b8a9e2429e93829b0d0164c21a6e8",
+    "url": "/img/cardboard-buttons-background.8a8b8a9e.jpg"
+  },
+  {
+    "revision": "095a096f2ac02ae54b561c6a769dcbaa",
+    "url": "/img/cards/thanks.jpg"
+  },
+  {
+    "revision": "51fe2dc3939858a14fbc8501f202fa97",
+    "url": "/img/cards/location.jpg"
+  },
+  {
+    "revision": "2da9fc21d046681da6ba43dcb1258421",
+    "url": "/img/cards/save-the-date.jpg"
+  },
+  {
+    "revision": "888f5d29c8a89559071b8d575cf0e66a",
+    "url": "/img/cards/info-horizontal.jpg"
+  },
+  {
+    "revision": "30eda5169f23d244fd6cf28f24368457",
+    "url": "/img/cards/404.jpg"
+  },
+  {
+    "revision": "74354c3c143fbdf2974327d5e21eb691",
+    "url": "/img/cards/gift-boxes.jpg"
+  },
+  {
+    "revision": "91db641441c4f293d73411842619af0b",
+    "url": "/img/cards/contact.jpg"
+  },
+  {
+    "revision": "246152eb955647eb0b9b15ef299a9154",
+    "url": "/img/cards/info-vertical.jpg"
+  },
+  {
+    "revision": "487f7a5f910b87aff21a932e8811b598",
+    "url": "/fonts/TofinoPersonal-Book.487f7a5f.ttf"
+  },
+  {
+    "revision": "05f0879a9ed6ec1234df24f31b8ed65c",
+    "url": "/fonts/PWSmallIcons.05f0879a.ttf"
+  },
+  {
+    "revision": "383f3bd8fff20445460b96bf5cb50050",
+    "url": "/fonts/PHONES.383f3bd8.TTF"
+  },
+  {
+    "revision": "39db6fd31cfb25d5442fcd9edf44c20c",
+    "url": "/fonts/KBREINDEERGAMES.39db6fd3.ttf"
+  },
+  {
+    "revision": "fe4792d482196a50cf9ae0d9d90b6493",
+    "url": "/favicon-old.ico"
+  },
+  {
+    "revision": "79ead0242c373ab15a6e9df332b29f63",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "a2fa44b84766bfee3178834a8ab70fa0",
+    "url": "/css/buttons.css"
+  },
+  {
+    "revision": "b62334fbd3c092158aae",
+    "url": "/css/app.0d2369f8.css"
+  }
+];
